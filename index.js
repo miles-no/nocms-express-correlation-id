516c9936cc1fd75ecca3077c1b21fdbca373ab92
Jon Arild Tørresdal <jon@torresdal.net>
2017-10-11 22:34:27 +0200

    fix: handle empty next function

---
+++ index.js
@@ -0,0 +1,20 @@
+'use strict'
+
+// Based on https://github.com/toboid/express-correlation-id
+
+const correlator = require('correlation-id');
+
+module.exports = (header = 'X-Correlation-ID') => {
+  return (req, res, next) {
+    req.correlationId = correlator.getId;
+
+    const id = req.get(header);
+    if (id) {
+      correlator.withId(id, next || () => {});
+    } else {
+      correlator.withId(next || () => {});
+    }
+  }
+}
+
+module.exports.getId = correlator.getId;
